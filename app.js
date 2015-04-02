63995f5ba571fd60bd915933b3e6e8a29464a7fe
Martin <marquezx@163.com>
2015-04-02 22:49:47 +0800

    group6

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var debug = require('./routes/debug');
-
 
 var app = express();
 
