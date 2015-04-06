89b30b4b478e50f117f29e1f26f8f3f4b77a3575
Zw <knight.zhangwengame@gmail.com>
2015-04-06 21:35:30 +0800

    frontend and some backend

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var debug = require('./routes/debug');
 
+
 var app = express();
 
 var settings = require('./settings');
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/', course);
 app.use('/users', users);
 app.use('/debug', debug);
 
