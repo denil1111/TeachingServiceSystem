cb68c82e1dcddbec47869d8297bf10a0c9bf1ec5
DarryO <lihaodemail@foxmail.com>
2015-04-10 19:58:37 +0800

    add the remote database

--- lhtest/index.js
+++ lhtest/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var dbtest = require('./dbtest');
-var findByModel = require('./findByModel');
 /* GET home page. */
 router.get('/',function(req, res, next) {
   res.send('this is a test');
