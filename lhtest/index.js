c479a4b3b1fd7150b1b25cf08dce9f14a05fbc07
DarryO <lihaodemail@foxmail.com>
2015-04-10 15:25:48 +0800

    Demo for group6 db

---
+++ lhtest/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var dbtest = require('./dbtest');
+var findByModel = require('./findByModel');
+/* GET home page. */
+router.get('/',function(req, res, next) {
+  res.send('this is a test');
+});
+router.use('/dbtest',dbtest);
+//router.use('/findByModel',findByModel);
+module.exports = router;
