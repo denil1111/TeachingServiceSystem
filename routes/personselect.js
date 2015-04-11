76af2c7b272cc14f7e47c63ec48d185bab1d091b
ele <elegabriel@163.com>
2015-04-12 00:56:46 +0800

    slove the ejs output problem 201504120056

--- routes/personselect.js
+++ routes/personselect.js
@@ -2,9 +2,22 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
+var person_data_initial=[];
+person_data_initial.push({
+	username:'ele',
+	status:'s',
+	sex:'s',
+	age:'a',
+	major:'m',
+	college:'c',
+	title:'t',
+	tel:'t',
+	email:'e'
+});
+
 router.get('/personselect', function(req, res, next) {
     res.render('personselect',{
-    	person_data:'',
+    	person_data:person_data_initial,
         selectresult:'请提交表单'
     });
 });
@@ -16,13 +29,15 @@ router.post('/personselect',function(req, res, next){
 	var CollectionName = 'people';
 	var PersonModel = db.model('PersonModel',PersonSchema,CollectionName);
 
-	PersonModel.findbyname(req.body.username1, function(error, data){
+	PersonModel.findbyname(req.body.username, function(error, data){
 		if(error) {
 			console.log('find error!'+error);
 		} else {
 			console.log('find ok!'+data);
 		}
 		console.log('data : '+data);
+		var username=data[0].username;
+		console.log('username : '+username);
 		res.render('personselect',{
 			selectresult:'搜索结果：',
 			person_data: data
