b4e319a2fb644be9c14f9b638b8797551814867f
Gnnng <gnnnnng@gmail.com>
2015-04-11 21:51:57 +0800

    Add sub route 'resource'

---
+++ routes/resource.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Gnnng on 4/10/15.
+ */
+var express = require('express');
+var router = express.Router();
+
+// TODO: wait to split those routes into separate files
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Resource' });
+});
+
+router.get('/cloud', function(req, res, next) {
+    res.render('index', { title: 'Cloud' });
+});
+
+router.get('/course', function(req, res, next) {
+    res.render('index', { title: 'Course' });
+});
+
+router.get('/config', function(req, res, next) {
+    res.render('index', { title: 'Config' });
+});
+
+module.exports = router;
