452e5ea29dc15bc078a4e1303ef75225063a1950
ele <elegabriel@163.com>
2015-04-11 14:05:46 +0800

    add insert.js insert.ejs 201504111405

---
+++ db/group1db/newSchema.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+// Schema 结构
+var personSchema = new mongoose.Schema({
+    username	: {type : String, default : '匿名用户'},
+    status		: {type : String, default : '学生'},
+    age			: {type : Number},
+    college		: {type : String},
+    tel			: {type : String},
+    email		: {typr : String},
+    time     : {type : Date, default: Date.now},
+    
+});
+personSchema.methods.findbyusername = function(username, callback) {
+    return this.model('mongoose').find({username: username}, callback);
+}
+/*
+mongooseSchema.statics.findbytitle = function(title, callback) {
+    return this.model('mongoose').find({title: title}, callback);
+}
+*/
+module.exports=personSchema;
--- routes/insert.js
+++ routes/insert.js
@@ -1,57 +1,49 @@
 var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-var multer = require('multer');
+var router = express.Router();
+var mongoose = require('mongoose'); 
+//var bodyParser = require('body-parser');
+//var multer = require('multer');
 
-var  mongodb = require('mongodb');
-var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
-var  db = new mongodb.Db('mydb', server, {safe:true});
+//var  mongodb = require('mongodb');
+//var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
+//var  db = new mongodb.Db('mydb', server, {safe:true});
 
-app.set('views',__dirname);
-app.set('view engine','html');
-app.engine('.html',require('ejs').__express);
+//app.set('views',__dirname);
+//app.set('view engine','html');
+//app.engine('.html',require('ejs').__express);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(multer());
 
-app.get('/insert', function(req, res) {
-    res.render('insert');
+app.use('/',course);
+
+router.get('/insert', function(req, res) {
+    res.render('insert',{
+        username,
+        status:'学生',
+        age,
+        college,
+        tel,
+        email
+    });
 });
 
 app.post('/insert',function(req,res){
     console.log("post");
-    var tmp = {userid: req.body.userid, password: req.body.password};
-    console.log(tmp);
-    
-    var tmp = {userid:req.body.userid,username:req.body.username,userdegree:req.body.userdegree,
-    	usercolledge:req.body.usercolledge,usertel:req.body.usertel,useremial:req.body.useremial};
-    db.open(function(err,db){
-        if(!err){
-            console.log("open db ok!");
-            db.collection('mycoll',{safe:true},function(err,collection){
-                if(err){
-                    console.log("open mycoll err!");
-                }
-                else{
-                    console.log("open collection ok!");
-                    collection.insert([tmp],{safe:true},function(err,result){
-                    	if(err){
-                    		console.log("insert err!");
-                    	}
-                    	else{
-                    		console.log("insert ok!");
-                    		console.log(result);
-                    	}
-                    });
-                }
-            });
-        }
-        else{
-            console.log("open db err");
-        }
+    var db = mongoose.createConnection('mongodb://127.0.0.1:27017/mydb');
+    var personSchema = require('../db/group1db/newSchema');
+    var personModel = db.model('person',personSchema);
+    var personEntity = new personModel({
+        username : req.body.username,
+        status : req.body.status,
+        age : req.body.age,
+        college : req.body.college,
+        tel : req.body.tel,
+        email : req.body.email
     });
+    
+    personModel.insertByModel('');
 });
 
 var server =app.listen(3000,function(){
