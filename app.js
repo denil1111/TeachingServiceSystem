20d0312fd251c0db2616b41aa13fdd391db4c5ee
denil1111 <zyh.work@gmail.com>
2015-04-12 19:54:40 +0800

    global.db in use and new routes model

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var debug = require('./routes/debug');
 
 var app = express();
 
-// view engine setup
+var settings = require('./settings');
+var mongoose = require('mongoose');
+global.db       = mongoose.createConnection(settings.db.connect);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/debug', debug);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+// var info = require()
+// var arrange = require()
+// var select = require()
+// var resource = require()
+// var test = require()
+// var score = require()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+// router.get('/info', info);
+// router.get('/arrange', arrange);
+// router.get('/select', select);
+// router.get('/resource', resource);
+// router.get('/test', test);
+// router.get('/score', score);
 
 module.exports = router;
