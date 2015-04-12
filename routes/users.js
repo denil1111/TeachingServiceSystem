98aa8e956823542fd3970a6ef0016877a873af24
denil1111 <zyh.work@gmail.com>
2015-04-12 19:58:14 +0800

    delete something wrong in user.js and add settings.js into gitignore

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-var mongoose = require('mongoose');
-var db       = mongoose.createConnection('mongodb://127.0.0.1:27017/NodeJS');// 链接错误
-db.on('error', function(error) {
-    console.log(error);
-});
 
 module.exports = router;
