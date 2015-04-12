582f9733033682c2f96d0ca76c220c49faad08a4
Martin <marquezx@163.com>
2015-04-12 19:29:05 +0800

    add grades

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/', grades);
+app.use('/grades', grades);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
