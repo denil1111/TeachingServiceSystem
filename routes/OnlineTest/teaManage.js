8631197a03931a94bbdb84ef4dff60ce5506bfa5
crzzd <crzzd.stanley@gmail.com>
2015-04-13 09:32:02 +0800

    teaManage.ejs added

--- routes/OnlineTest/teaManage.js
+++ routes/OnlineTest/teaManage.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-	res.render('OnlineTest/teaManage');
+	res.render('OnlineTest/teaManage',{
+		name: '老程序猿',
+		image: 'images/avatars/avatar1.jpg'
+	});
 });
 
 module.exports = router;
