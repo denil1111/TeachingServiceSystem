e697f7a2bbf87c5746b7b5d836bce53afd79cca6
Martin <marquezx@163.com>
2015-04-13 20:08:36 +0800

    use settings

--- routes/grades.js
+++ routes/grades.js
@@ -1,8 +1,26 @@
+
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
+var mongo = require('../settings');
+var dbb = new mongo();
+var db = dbb.module();
+var mongoose=require('mongoose')
+var Schema = new mongoose.Schema({
+       name: String,
+       score:String
+});
+
+var gradesDB = db.model('grades',Schema);
+
 
 router.get('/', function(req, res, next) {
+  grades.find(function(error,docs){
+    if(error){
+        console.log(error);
+        return;
+    }
+  
+  
   res.render('grades', {
   	name: '程序员', 
   	image: 'images/avatars/avatar1.jpg',
@@ -11,9 +29,10 @@ router.get('/', function(req, res, next) {
   	total_b:'24',
   	b:'4,6,2,4,6,2,0',
   	total_credits:'24',
-  	credits:'4,6,2,4,6,2,0'
+  	credits:'4,6,2,4,6,2,0',
+    data:docs
   });
 });
-  
+});  
 
 module.exports = router;
