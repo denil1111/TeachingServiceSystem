675244426e0286b1380d740895063f54ffc6c911
lyt9304 <513535169@qq.com>
2015-04-13 23:10:42 +0800

    pages first version complete

--- routes/resource.js
+++ routes/resource.js
@@ -5,20 +5,46 @@ var express = require('express');
 var router = express.Router();
 
 // TODO: wait to split those routes into separate files
-router.get('/', function(req, res, next) {
-  res.render('myresource', { title: 'Resource' });
+router.get('/myresource', function(req, res, next) {
+  res.render('myresource',{});
 });
 
-router.get('/cloud', function(req, res, next) {
-  res.render('index', { title: 'Cloud' });
+router.get('/info', function(req, res, next) {
+  res.render('courseInfo',{});
 });
 
-router.get('/course', function(req, res, next) {
-  res.render('index', { title: 'Course' });
+router.get('/feedback', function(req, res, next) {
+  res.render('feedback',{});
 });
 
-router.get('/config', function(req, res, next) {
-  res.render('index', { title: 'Config' });
+router.get('/homework', function(req, res, next) {
+  res.render('homework',{});
 });
+
+router.get('/resource', function(req, res, next) {
+  res.render('resource',{});
+});
+
+router.get('/search', function(req, res, next) {
+  res.render('search', {});
+});
+
+router.get('/admin_changeinfo', function(req, res, next) {
+  res.render('admin_changeInfo', {});
+});
+
+router.get('/admin_feedback', function(req, res, next) {
+  res.render('admin_feedback', {});
+});
+
+router.get('/admin_homework', function(req, res, next) {
+  res.render('admin_homework', {});
+});
+
+router.get('/admin_resource', function(req, res, next) {
+  res.render('admin_resource', {});
+});
+
+
 
 module.exports = router;
