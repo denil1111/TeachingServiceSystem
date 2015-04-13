b0caadfbeb1d265666161311e51d67c12a24ebb1
Martin <marquezx@163.com>
2015-04-13 17:08:36 +0800

    fix bug in app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var grades= require('./routes/grades');
+var debug = require('./routes/debug');
 
 var app = express();
 
