8105160fbf205b781aa51be844943080955217ee
lyt9304 <513535169@qq.com>
2015-04-13 22:20:45 +0800

    pages for admin

--- routes/resource.js
+++ routes/resource.js
@@ -6,19 +6,19 @@ var router = express.Router();
 
 // TODO: wait to split those routes into separate files
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Resource' });
+    res.render('index', { title: 'Resource' });
 });
 
 router.get('/cloud', function(req, res, next) {
-  res.render('index', { title: 'Cloud' });
+    res.render('index', { title: 'Cloud' });
 });
 
 router.get('/course', function(req, res, next) {
-  res.render('index', { title: 'Course' });
+    res.render('index', { title: 'Course' });
 });
 
 router.get('/config', function(req, res, next) {
-  res.render('index', { title: 'Config' });
+    res.render('index', { title: 'Config' });
 });
 
 module.exports = router;
