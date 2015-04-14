047b6fb5f6cfa9fee8ba6f041eef0f18f0d78305
Martin <marquezx@163.com>
2015-04-14 09:02:59 +0800

    add a new page grades_analysis

--- routes/grades.js
+++ routes/grades.js
@@ -38,4 +38,21 @@ gradesDB.find(function(error,docs){
 });
 });  
 
+router.get('grades_analysis',function(req, res, next) {
+  res.render('grades', {
+  	name: '程序员', 
+  	image: 'images/avatars/avatar1.jpg',
+  	total_a:'12',
+  	a:'2,3,1,2,3,1,0',
+  	total_b:'24',
+  	b:'4,6,2,4,6,2,0',
+  	total_credits:'24',
+  	credits:'4,6,2,4,6,2,0',
+  });
+  }); 
+
+
+
+
+
 module.exports = router;
