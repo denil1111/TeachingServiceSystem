1297a0fb4b33f1b3654645c74275542b79fa5be8
Gnnng <gnnnnng@gmail.com>
2015-04-17 18:01:18 +0800

    Use busboy and gridfs-stream

--- app.js
+++ app.js
@@ -4,16 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var busboy = require('connect-busboy');
+var mongoose = require('mongoose');
+var Grid = require('gridfs-stream');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var debug = require('./routes/debug');
+
 
 var app = express();
 
 var settings = require('./settings');
-var mongoose = require('mongoose');
-global.db       = mongoose.createConnection(settings.db.connect);
+
+//NOTE: use mongoose.connect insteadof mongoose.createConnection
+mongoose.connect(settings.db.connect);
+
+//NOTE: These routes requires must be placed after mongoose.connect
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var debug = require('./routes/debug');
+
+//this method deprecated, please refer to NOTE
+//global.db       = mongoose.createConnection(settings.db.connect);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +31,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(busboy());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
