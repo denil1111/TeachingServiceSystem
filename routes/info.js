b73278d68f4e2474592dcd5dd960a48bc3942156
ele <elegabriel@163.com>
2015-04-22 10:08:26 +0800

    before checkout master

--- routes/info.js
+++ routes/info.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var router = express.Router();
+var info = express.Router();
 
 var personinsert = require('./personinsert');
 var personselect = require('./personselect');
 var courseinsert = require('./courseinsert');
 var courseselect = require('./courseselect');
 
-router.use('/personinsert', personinsert);
-router.use('/personselect', personselect);
-router.use('/courseinsert', courseinsert);
-router.use('/courseselect', courseselect);
+info.use('/', personinsert);
+info.use('/', personselect);
+info.use('/', courseinsert);
+info.use('/', courseselect);
 
-module.exports = router;
+module.exports = info;
