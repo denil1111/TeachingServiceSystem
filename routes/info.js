eb426ff5b5c2dd6974e91e7a149e098e0a2aef21
ele <elegabriel@163.com>
2015-05-10 19:07:00 +0800

    tmp commit for info

--- routes/info.js
+++ routes/info.js
@@ -7,10 +7,17 @@ var courseinsert = require('./courseinsert');
 var courseselect = require('./courseselect');
 var login =require('./login');
 
+info.use('/',login);
+// info.all('/',isLoggedIn);
 info.use('/', personinsert);
 info.use('/', personselect);
 info.use('/', courseinsert);
 info.use('/', courseselect);
-info.use('/',login);
+
+// function isLoggedIn(req,res,next){
+// 	if(req.isAuthenticated())
+// 		return next();
+// 	res.redirect('/info/login');
+// }
 
 module.exports = info;
