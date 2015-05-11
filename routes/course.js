5bc1e434b40a058c954e0ecb5d1e21af3691c5a0
爱叶言 <yangmh.meldoy@gmail.com>
2015-05-11 15:07:50 +0800

    可以进行选课

--- routes/course.js
+++ routes/course.js
@@ -55,7 +55,7 @@ router.post('/course_search', function(req, res, next) {
 var my_course=[];
 my_course.push({
   ID:"00001",
-  name:'程序员的自我修养',
+  name:'计算机组成',
   teacher:'XX1',
   semaster:'春',
   time:'周一 345',
@@ -65,7 +65,7 @@ my_course.push({
 //注意所有课程的time日期格式，多个上课时间之间以空格分隔，共有以下情况：
 //晚上的课程格式为 Mon night
 //其余为 周一 123 或 周一 12 以此类推
-my_course.push({ID:"00002",name:'论程序员的把妹精神',teacher:'XX2', semaster:'春夏',time:'周一 12 周二 345',campus:'玉泉',room:'曹西-101'});
+my_course.push({ID:"00002",name:'软件工程',teacher:'XX2', semaster:'春夏',time:'周一 12 周二 345',campus:'玉泉',room:'曹西-101'});
 /* GET home page. */
 router.get('/my_course', function(req, res, next) {
   console.log(my_course.ejs);
@@ -134,8 +134,12 @@ dev_plan_elec_class.push({classification:"程序员卖蠢系列",min_credits:"20
 dev_plan_elec_class.push({classification:"奇技淫巧",min_credits:"20",credits:"22"});
 
 var dev_plan_req=[];//必修课变量
-dev_plan_req.push({ID:"0011345",name:"如何正确的把妹",time:"大三 秋冬",credit:"4.5",complete:false})
-dev_plan_req.push({ID:"0011346",name:"如何正确的犯蠢",time:"大三 秋冬",credit:"4.5",complete:false})
+dev_plan_req.push({ID:"0011345",name:"如何正确的把妹",time:"大三 秋冬",credit:"4.5",complete:false});
+dev_plan_req.push({ID:"0011346",name:"如何正确的犯蠢",time:"大三 秋冬",credit:"4.5",complete:false});
+var major=[];//专业
+major.push("计算机科学与技术");
+major.push("软件工程");
+major.push("自动化控制");
 router.get('/dev_plan', function(req, res, next) {
   console.log(course.ejs);
   res.render('dev_plan', {
@@ -147,6 +151,26 @@ router.get('/dev_plan', function(req, res, next) {
     b:'4,6,2,4,6,2,0',
     total_credits:'24',
     credits:'4,6,2,4,6,2,0',
+    major:major,
+    dev_plan_gen:dev_plan_gen,
+    dev_plan_elec:dev_plan_elec,
+    dev_plan_elec_class:dev_plan_elec_class,
+    dev_plan_req:dev_plan_req
+  });
+});
+//获取专业信息
+router.post('/dev_plan', function(req, res, next) {
+  console.log(req.body);
+  res.render('dev_plan', {
+    name: '程序员', 
+    image: 'images/avatars/avatar3.jpg',
+    total_a:'12',
+    a:'2,3,1,2,3,1,0',
+    total_b:'24',
+    b:'4,6,2,4,6,2,0',
+    total_credits:'24',
+    credits:'4,6,2,4,6,2,0',
+    major:major,
     dev_plan_gen:dev_plan_gen,
     dev_plan_elec:dev_plan_elec,
     dev_plan_elec_class:dev_plan_elec_class,
@@ -158,8 +182,8 @@ router.get('/dev_plan', function(req, res, next) {
 //credits的含义为培养方案中已包含的总学分数量
 //增加变量my_dev_plan_gen，其含义为我的培养方案中制定的通识课
 var my_dev_plan_gen=[];
-my_dev_plan_gen.push({ID:"0015345",name:"人生哲学漫谈",time:"大三 夏",credit:"2.5",complete:false,classi:"沟通与领导类"});
-my_dev_plan_gen.push({ID:"0015345",name:"脊椎病的预防与治疗",time:"大三 夏",credit:"1.5",complete:false,classi:"体育"});
+my_dev_plan_gen.push({ID:"0015345",name:"Java应用基础",time:"大三 夏",credit:"2.5",complete:false,classi:"沟通与领导类"});
+my_dev_plan_gen.push({ID:"0015345",name:"B/S软件设计",time:"大三 夏",credit:"1.5",complete:false,classi:"体育"});
 
 router.get('/my_dev_plan', function(req, res, next) {
   console.log(course.ejs);
@@ -222,3 +246,37 @@ router.post('/choose', function(req, res, next) {
 });
 
 module.exports = router;
+
+//进入选课系统
+var course=[];//该课程号对应的所有不同老师、时间段的课程
+course.push({teacher:"xxx",campus:"玉泉",time:"周一12 周三345",room:"曹西502",language:"双语",remain:20,all:40,waiting:30});
+course.push({teacher:"xxx",campus:"玉泉",time:"周一12 周三345",room:"曹西502",language:"双语",remain:20,all:40,waiting:30});
+course.push({teacher:"xxx",campus:"玉泉",time:"周一12 周三345",room:"曹西502",language:"双语",remain:20,all:40,waiting:30});
+router.get('/:courseID', function(req, res, next){
+	//课程号
+	var course_id = req.params.courseID;
+  res.render('choose', {
+    course_id:course_id,
+    course_name:"软件工程",
+    credits:2.5,
+    course:course,
+    my_choice:1,//记录登陆人员选择的是哪个选项
+    name: '程序员', 
+    image: 'images/avatars/avatar3.jpg'
+  });
+});
+
+router.post('/:courseID', function(req, res, next){
+	console.log(req.body);
+  //课程号
+	var course_id = req.params.courseID;
+  res.render('choose', {
+    course_id:course_id,
+    course_name:"软件工程",
+    credits:2.5,
+    course:course,
+    my_choice:1,//记录登陆人员选择的是哪个选项
+    name: '程序员', 
+    image: 'images/avatars/avatar3.jpg'
+  });
+});
