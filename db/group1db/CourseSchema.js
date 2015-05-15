fb07039b421f453b265695798391006f8d2315b6
ele <elegabriel@163.com>
2015-05-15 15:55:16 +0800

    CourseSchema <-add courseterm campus

--- db/group1db/CourseSchema.js
+++ db/group1db/CourseSchema.js
@@ -3,11 +3,13 @@ var mongoose = require('mongoose');
 var CourseSchema = new mongoose.Schema({
 	courseid	: {type : String},	//课程id
     coursename  : {type : String},	//课程名称
+    courseterm  : {type : String},  //课程学期
     coursetime	: {type : String},	//上课时间
     coursescore	: {type : Number},	//课程学分
     teacher     : {type : String},	//授课老师
     examtime	: {type : String},	//考试时间
     room        : {type : String},	//上课教室
+    campus      : {type : String},  //上课校区
     college     : {type : String},	//开课学院
     time     : {type : Date, default: Date.now}	//创建时间
     
