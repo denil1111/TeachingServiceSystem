73dc74dc0d44a8184c5cdc94f8496727fa6e5e2d
ele <elegabriel@163.com>
2015-05-16 09:59:02 +0800

    PersonSchema update

--- db/group1db/PersonSchema.js
+++ db/group1db/PersonSchema.js
@@ -1,17 +1,18 @@
 var mongoose = require('mongoose');
 // Schema 结构
 var PersonSchema = new mongoose.Schema({
-    username	: {type : String, default : '匿名用户'},
-    status		: {type : String, default : '学生'},
-    sex			: {type : String},
-    cstlist     : [],//课程列表
-    age			: {type : Number},
+    userid      : {type : String},  //学工号 unique
+    username	: {type : String, default : '匿名用户'},   //用户名
+    status		: {type : String, default : '学生'},      //用户身份 系统管理员/教务管理员/教师/学生
+    sex			: {type : String},    //性别
+    cstlist     : [],   //课程列表
+    age			: {type : Number},    //年龄
     major       : {type : String},  //专业
     college		: {type : String}, //学院
     title       : {type : String, default : '无'},   //职称
-    tel			: {type : String},
-    email		: {type : String},
-    time     : {type : Date, default: Date.now}
+    tel			: {type : String},    //电话
+    email		: {type : String},   //邮件
+    time     : {type : Date, default: Date.now} //创建时间
     
 });
 /*
