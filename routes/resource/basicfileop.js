39e972af1fdac6f9637e9a2cbef89c1c78e48e4d
dodoleon <gaotaohu@163.com>
2015-05-21 13:14:46 +0800

    fix som bug of file

--- routes/resource/basicfileop.js
+++ routes/resource/basicfileop.js
@@ -21,6 +21,46 @@ function fileupload(req, callback) {
   });
 };
 
+function filedowloadbyname(fileName,res,next,callback) {
+  var opts = {
+    filename: fileName
+  };
+  gfs.exist(opts, function(err, found) {
+    if (err)
+      return next(err);
+    if (found) {
+      var rs = gfs.createReadStream(opts);
+      console.log(rs);      
+      res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
+      res.setHeader('Content-type', 'text/plain');
+      rs.pipe(res);
+    } else {
+      next(new Error('File ' + fileName + ' not found'));
+    }
+  });
+};
+
+function filedowloadbyid(fileid,filename,res,next,callback) {
+  var opts = {
+    _id: fileid
+  };
+  gfs.exist(opts, function(err, found) {
+    if (err)
+      return next(err);
+    if (found) {
+      var rs = gfs.createReadStream(opts);
+      console.log(rs);      
+      res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+      res.setHeader('Content-type', 'text/plain');
+      rs.pipe(res);
+    } else {
+      next(new Error('File id:' + fileid + ' not found'));
+    }
+  });
+};
+
 var FILE = {};
 FILE.upload = fileupload;
+FILE.dowloadbyname = filedowloadbyname;
+FILE.dowloadbyid = filedowloadbyid;
 module.exports = FILE;
