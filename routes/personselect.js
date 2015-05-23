6112e04d47636014b835d43229cd6a4ee71f0880
ele <elegabriel@163.com>
2015-05-24 00:56:41 +0800

    改动非常多：通过id和password登录，登录后如果是系统管理员，则进入信息管理页面，如果是其他用户，进入个人信息页面（新增）；完成了登录界面的UI设置；在PersonSchema中添加了password和photo，可设置密码和照片；添加了新增用户时，用户信息条件过滤

--- routes/personselect.js
+++ routes/personselect.js
@@ -8,18 +8,42 @@ var PersonModel = require('../db/group1db/PersonModel');
 
 router.get('/personselect', function(req, res, next) {
 	if(!req.session.user){return res.redirect('login');}
-    res.render('personselect',{
-    	name: '程序员', 
-    	image: 'images/avatars/avatar3.jpg',
-    	total_a:'12',
-    	a:'2,3,1,2,3,1,0',
-    	total_b:'24',
-    	b:'4,6,2,4,6,2,0',
-    	total_credits:'24',
-    	credits:'4,6,2,4,6,2,0',
+	console.log(req.session.user);
+	console.log(req.session.user[0].username);
+	PersonModel.findbyname(req.session.user[0].username,function (err, user) {
+        if (err) {
+            console.log('find error!'+error);
+        }
+        if (!user) {
+            console.log('user not found!');
+        }
+        res.render('personselect',{
+			name: '程序员', 
+			image: 'images/avatars/avatar3.jpg',
+			total_a:'12',
+			a:'2,3,1,2,3,1,0',
+			total_b:'24',
+			b:'4,6,2,4,6,2,0',
+			total_credits:'24',
+			credits:'4,6,2,4,6,2,0',
 
-    	person_data:tmp
-    });
+			person_data: user
+		});
+    }); 
+
+	// console.log(req.session.user);
+ //    res.render('personselect',{
+ //    	name: '程序员', 
+ //    	image: 'images/avatars/avatar3.jpg',
+ //    	total_a:'12',
+ //    	a:'2,3,1,2,3,1,0',
+ //    	total_b:'24',
+ //    	b:'4,6,2,4,6,2,0',
+ //    	total_credits:'24',
+ //    	credits:'4,6,2,4,6,2,0',
+
+ //    	 person_data:req.session.user//person_data:tmp
+ //    });
 });
 
 router.post('/personselect',function(req, res, next){
@@ -37,6 +61,7 @@ router.post('/personselect',function(req, res, next){
 			console.log('user not found!');
 		}
 		console.log("user : "+user.length);
+		console.log('user : '+user);
 		res.render('personselect',{
 			name: '程序员', 
 			image: 'images/avatars/avatar3.jpg',
