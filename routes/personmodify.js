6e5cf0d735d399393e3531006759390bedca683d
denil1111 <zyh.work@gmail.com>
2015-05-25 11:31:21 +0800

    Revert "Merge branch 'master' of https://gitcafe.com/ZJUSEGroupA/TeachingServiceSystem into group4"
    This reverts commit 8dff06f0873f19cb497a71030fd956c35778c8bb, reversing
    changes made to 51f7164276d9110b82c5dbf9e922f9af03124d15.

--- routes/personmodify.js
+++ routes/personmodify.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var formidable = require('formidable');
-var fs = require('fs');
+var mongoose = require('mongoose'); 
 
 router.get('/personmodify', function(req, res,next) {
     if(!req.session.user){return res.redirect('login');}
@@ -26,125 +24,55 @@ router.post('/personmodify',function(req,res,next){
     var PersonModel = require('../db/group1db/PersonModel');
     // var CollectionName = 'people';
     // var PersonModel = db.model('PersonModel',PersonSchema,CollectionName);
-    var form = new formidable.IncomingForm(); //创建上传表单
-    form.encoding = 'utf-8';		//设置编辑
-    form.uploadDir = 'public/images';//设置上传目录
-    form.keepExtensions = true;	 // 保留后缀
-    form.maxFieldsSize = 2 * 1024 * 1024;//文件大小
-    form.parse(req, function(err, fields, files)
-    {
-        if (err) {
-            res.locals.error = err;
-            console.log("Err:formidable.parse fail");
-            res.render('personmodify',{
-                name: '程序员',
-                image: 'images/avatars/avatar3.jpg',
-                total_a:'12',
-                a:'2,3,1,2,3,1,0',
-                total_b:'24',
-                b:'4,6,2,4,6,2,0',
-                total_credits:'24',
-                credits:'4,6,2,4,6,2,0',
-                insertresult:'表单解析失败'
-            });
-            fs.unlink(files.fulAvatar.path);
-            return;
-        }
-        var extName = '';//后缀名
-        switch (files.fulAvatar.type)
-        {
-            case 'image/pjpeg':				extName = 'jpg';break;
-            case 'image/jpeg':				extName = 'jpg';break;
-            case 'image/png':				extName = 'png';break;
-            case 'image/x-png':				extName = 'png';break;
-        }
-        if(extName.length == 0){
-            console.log("Err:invalid image type");
-            res.render('personmodify',{
-                name: '程序员',
-                image: 'images/avatars/avatar3.jpg',
-                total_a:'12',
-                a:'2,3,1,2,3,1,0',
-                total_b:'24',
-                b:'4,6,2,4,6,2,0',
-                total_credits:'24',
-                credits:'4,6,2,4,6,2,0',
-                insertresult:'只支持png和jpg格式图片'
-            });
-            fs.unlink(files.fulAvatar.path);
-            return;
-        }
-        //console.log(files.fulAvatar);
-        if(files.fulAvatar.size > 1000000){
-            console.log("Err:Too large image ");
-            res.render('personmodify',{
-                name: '程序员',
-                image: 'images/avatars/avatar3.jpg',
-                total_a:'12',
-                a:'2,3,1,2,3,1,0',
-                total_b:'24',
-                b:'4,6,2,4,6,2,0',
-                total_credits:'24',
-                credits:'4,6,2,4,6,2,0',
-                insertresult:'图片大小不能超过1000,000'
-            });
-            fs.unlink(files.fulAvatar.path);
-            return;
-        }
-        var data = fs.readFileSync(files.fulAvatar.path);
-        var b64data="data:image/gif;base64,"+data.toString('base64');
-        fs.unlink(files.fulAvatar.path);
-        var doc = {
-            photo: b64data,
-            username : fields.username,
-            status : fields.status,
-            sex : fields.sex,
-            age : fields.age,
-            major : fields.major,
-            college : fields.college,
-            title : fields.title,
-            tel : fields.tel,
-            email : fields.email
-        };
-        console.log("doc:"+doc.username);
-        PersonModel.modifybyname(doc,function(err,data){
-            if(err){
-                console.log("modify err : "+err);
-                res.render('personmodify',{
-                    name: '程序员',
-                    image: 'images/avatars/avatar3.jpg',
-                    total_a:'12',
-                    a:'2,3,1,2,3,1,0',
-                    total_b:'24',
-                    b:'4,6,2,4,6,2,0',
-                    total_credits:'24',
-                    credits:'4,6,2,4,6,2,0',
+    var doc = {
+        username : req.body.username,
+        status : req.body.status,
+        sex : req.body.sex,
+        age : req.body.age,
+        major : req.body.major,
+        college : req.body.college,
+        title : req.body.title,
+        tel : req.body.tel,
+        email : req.body.email
+    };
 
-                    modifyresult:'用户修改失败！'
-                })
-            }
-            else{
-                console.log('data'+data);
-                console.log('Update Model OK!');
-                console.log(doc.username);
-                res.render('personmodify',{
-                    name: '程序员',
-                    image: 'images/avatars/avatar3.jpg',
-                    total_a:'12',
-                    a:'2,3,1,2,3,1,0',
-                    total_b:'24',
-                    b:'4,6,2,4,6,2,0',
-                    total_credits:'24',
-                    credits:'4,6,2,4,6,2,0',
+    console.log("doc:"+doc.username);
+    
+    PersonModel.modifybyname(doc,function(err,data){
+        if(err){
+            console.log("modify err : "+err);
+            res.render('personmodify',{
+                name: '程序员', 
+                image: 'images/avatars/avatar3.jpg',
+                total_a:'12',
+                a:'2,3,1,2,3,1,0',
+                total_b:'24',
+                b:'4,6,2,4,6,2,0',
+                total_credits:'24',
+                credits:'4,6,2,4,6,2,0',
 
-                    modifyresult:'用户修改成功！'
-                });
-            }
-        });
+                modifyresult:'用户修改失败！'
+            })
+        }
+        else{
+            console.log('data'+data);
+            console.log('Update Model OK!');
+            console.log(doc.username);
+            res.render('personmodify',{
+                name: '程序员', 
+                image: 'images/avatars/avatar3.jpg',
+                total_a:'12',
+                a:'2,3,1,2,3,1,0',
+                total_b:'24',
+                b:'4,6,2,4,6,2,0',
+                total_credits:'24',
+                credits:'4,6,2,4,6,2,0',
+
+                modifyresult:'用户修改成功！'
+            });
+        }
     });
-
-
-
+//    db.close();
 });
 
 module.exports = router;
