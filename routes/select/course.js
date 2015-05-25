613ca63185bc0733aaeaf6eb3e41850b64466e87
爱叶言 <yangmh.meldoy@gmail.com>
2015-05-25 17:22:40 +0800

    增加选课时间页面，对之前的一些问题进行调整

--- routes/select/course.js
+++ routes/select/course.js
@@ -115,7 +115,7 @@ my_course.push({course_id:"000001",ID:"00002",name:'软件工程',teacher:'XX2',
 router.get('/my_course', function(req, res, next) {
   console.log(my_course.ejs);
   res.render('select/my_course', {
-    type:2,//manager
+    type:1,//manager
     name: '程序员', 
     image: 'images/avatars/avatar3.jpg',
     total_a:'12',
@@ -448,5 +448,29 @@ router.get('/course_list/:courseID', function(req, res, next){
     students:students,
     name: '程序员', 
     image: '../images/avatars/avatar3.jpg'
+  });
+});
+
+//选课时间确定
+choose_time=[];
+choose_time.push({start_time:"2015/01/01",end_time:"2015/02/02",isChoose:true,isCancell:true,ID:1});
+choose_time.push({start_time:"2015/03/01",end_time:"2015/04/02",isChoose:true,isCancell:false,ID:2});
+//ID为主键
+router.get('/time', function(req, res, next) {
+  console.log(course.ejs);
+  res.render('select/time', {
+    type:2,//manager
+    name: '程序员', 
+    image: 'images/avatars/avatar3.jpg',
+    choose_time:choose_time
+  });
+});
+router.post('/time', function(req, res, next) {
+  console.log(req.body);
+  res.render('select/time', {
+    type:2,//manager
+    name: '程序员', 
+    image: 'images/avatars/avatar3.jpg',
+    choose_time:choose_time
   });
 });
