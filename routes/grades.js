e465ccdedf14410d0a8553a0af0c54e43f614eae
Martin <marquezx@163.com>
2015-05-25 09:10:51 +0800

    prepare for update coursesSchema

--- routes/grades.js
+++ routes/grades.js
@@ -21,13 +21,15 @@ criteria.userid = req.session.user[0].userid;
 
 // console.log(req.session.user[0].userid);
 
+if(req.session.user[0].status=="student"){
+
  gradesDB.find(criteria,function(error,docs){
      if(error){
          console.log(error);
          return;
      }
    
-   res.render('grades/grades', {
+   res.render('grades/student_grades', {
    	name: '程序员', 
    	image: 'images/avatars/avatar1.jpg',
    	total_a:'12',
@@ -36,23 +38,53 @@ criteria.userid = req.session.user[0].userid;
    	b:'4,6,2,4,6,2,0',
    	total_credits:'24',
    	credits:'4,6,2,4,6,2,0',
-     data:docs
+    data:docs
    });
  });
+}
+else if (req.session.user[0].status=="teacher"){
+  
+  res.render('grades/teacher_classlist', {
+    name: '程序员', 
+    image: 'images/avatars/avatar1.jpg',
+    total_a:'12',
+    a:'2,3,1,2,3,1,0',
+    total_b:'24',
+    b:'4,6,2,4,6,2,0',
+    total_credits:'24',
+    credits:'4,6,2,4,6,2,0'
+   });
+}
 
+else if(req.session.user[0].status=="admin"){
+  
+  res.render('grades/admin_gradesaudit', {
+    name: '程序员', 
+    image: 'images/avatars/avatar1.jpg',
+    total_a:'12',
+    a:'2,3,1,2,3,1,0',
+    total_b:'24',
+    b:'4,6,2,4,6,2,0',
+    total_credits:'24',
+    credits:'4,6,2,4,6,2,0'
+   });
+}
 
 
 });  
 
 router.get('/gradesAnalysis',function(req, res, next) {
 
+if(!req.session.user){return res.redirect('../info/login');}
+
+
 gradesDB.find(function(error,docs){
     if(error){
         console.log(error);
         return;
     }
-  
-  res.render('grades/gradesAnalysis', {
+
+  res.render('grades/student_analysis', {
   	name: '程序员', 
   	image: 'images/avatars/avatar1.jpg',
   	total_a:'12',
@@ -60,8 +92,7 @@ gradesDB.find(function(error,docs){
   	total_b:'24',
   	b:'4,6,2,4,6,2,0',
   	total_credits:'24',
-  	credits:'4,6,2,4,6,2,0',
-    data:docs
+  	credits:'4,6,2,4,6,2,0'
   });
   }); 
 });  
@@ -69,13 +100,17 @@ gradesDB.find(function(error,docs){
 
 router.get('/testSearch',function(req, res, next) {
 
+
+if(!req.session.user){return res.redirect('../info/login');}
+
+
 gradesDB.find(function(error,docs){
     if(error){
         console.log(error);
         return;
     }
   
-  res.render('grades/testSearch', {
+  res.render('grades/student_test', {
   	name: '程序员', 
   	image: 'images/avatars/avatar1.jpg',
   	total_a:'12',
@@ -92,13 +127,16 @@ gradesDB.find(function(error,docs){
 
 router.get('/tutorial',function(req, res, next) {
 
+if(!req.session.user){return res.redirect('../info/login');}
+
+
 gradesDB.find(function(error,docs){
     if(error){
         console.log(error);
         return;
     }
   
-  res.render('grades/tutorial', {
+  res.render('grades/student_guide', {
   	name: '程序员', 
   	image: 'images/avatars/avatar1.jpg',
   	total_a:'12',
@@ -114,13 +152,16 @@ gradesDB.find(function(error,docs){
 
 router.get('/classLists',function(req, res, next) {
 
+if(!req.session.user){return res.redirect('../info/login');}
+
+
 gradesDB.find(function(error,docs){
     if(error){
         console.log(error);
         return;
     }
   
-  res.render('grades/classLists', {
+  res.render('grades/teacher_classlist', {
   	name: '程序员', 
   	image: 'images/avatars/avatar1.jpg',
   	total_a:'12',
@@ -137,13 +178,16 @@ gradesDB.find(function(error,docs){
 
 router.get('/classManagement',function(req, res, next) {
 
+if(!req.session.user){return res.redirect('../info/login');}
+
+
 gradesDB.find(function(error,docs){
     if(error){
         console.log(error);
         return;
     }
   
-  res.render('grades/classManagement', {
+  res.render('grades/teacher_classmanage', {
   	name: '程序员', 
   	image: 'images/avatars/avatar1.jpg',
   	total_a:'12',
@@ -159,13 +203,16 @@ gradesDB.find(function(error,docs){
 
 router.get('/gradesAudit',function(req, res, next) {
 
+if(!req.session.user){return res.redirect('../info/login');}
+
+
 gradesDB.find(function(error,docs){
     if(error){
         console.log(error);
         return;
     }
   
-  res.render('grades/gradesAudit', {
+  res.render('grades/admin_gradesaudit', {
   	name: '程序员', 
   	image: 'images/avatars/avatar1.jpg',
   	total_a:'12',
