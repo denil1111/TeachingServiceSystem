96a4fa9310e5ea8eae8ea46c04a111abefcff479
Martin <marquezx@163.com>
2015-05-27 00:06:02 +0800

    Add grades submit

--- routes/grades.js
+++ routes/grades.js
@@ -411,6 +411,54 @@ gradesDB.update({userid:req.body.userid,courseid:req.body.courseid},{$set:{score
 }); 
 
 
+router.post('/gradesSubmit',function(req, res, next) {
+
+if(!req.session.user){return res.redirect('../info/login');}
+
+console.log(req.body.courseid);
+
+CourseModel.update({courseid:req.body.courseid},{$set:{status:"off"}},function(error,back){
+    if(error){
+        console.log(error);
+        return;
+    }
+  
+        
+CourseModel.findbylist(req.session.user[0].cstlist,function(error,clist){
+    if(error){
+         console.log(error);
+         return;
+     }
+    var cliston=[];
+    var clistoff=[]; 
+    
+    for(var i=0;i<clist.length;i++){
+      if(clist[i].status=="on"){
+        cliston.push(clist[i]);
+      }
+      else if(clist[i].status=="off"){
+        clistoff.push(clist[i]);
+      }
+    }
+   
+    res.render('grades/teacher_classlist', {
+    name: '程序员', 
+    image: 'images/avatars/avatar1.jpg',
+    total_a:'12',
+    a:'2,3,1,2,3,1,0',
+    total_b:'24',
+    b:'4,6,2,4,6,2,0',
+    total_credits:'24',
+    credits:'4,6,2,4,6,2,0',
+    cliston: cliston,
+    clistoff:clistoff
+   });  
+   
+ });
+ });
+ }); 
+
+
 
 router.get('/gradesAudit',function(req, res, next) {
 
