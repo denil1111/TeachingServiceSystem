a8d225c1beb6b6ae777e2f381921027bfb98016b
mwindson <mwindson@163.com>
2015-05-29 16:27:44 +0800

    modify gradesDB

--- db/group6db/gradesDB.js
+++ db/group6db/gradesDB.js
@@ -1,15 +1,5 @@
 var mongoose = require('mongoose');
 
-// var Schema = new mongoose.Schema({
-// courseNumber: String,
-// courseName:String,
-// score:Number,
-// credit:Number,
-// gradePoint:Number,
-// secondScore:Number
-// });
-
-
 var studentGradeSchema = new mongoose.Schema({
 courseid    : String,  
 userid      : String,  
