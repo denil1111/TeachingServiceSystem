479ca62afcb0015612ba63c802429e15b60c80f8
HittaX <songzp.work.com>
2015-05-31 04:31:30 +0800

    X

--- routes/arrange/timetable_classroom.js
+++ routes/arrange/timetable_classroom.js
@@ -18,31 +18,33 @@ router.get('/timetable_classroom',function(req,res,next){
         b:'4,6,2,4,6,2,0',
         total_credits:'24',
         credits:'4,6,2,4,6,2,0',
-        classroom_data : tmp
+        course_data : tmp
     });
 });
 
 router.post('/timetable_classroom', function(req, res, next) {
     console.log("post:timetable_classroom");
-    CourseModel.findbyclassroom(req.body.campus,req.body.room, function(error,data){
-    if(error)
-	{
-		console.log('find error!'+error);
-	}
-	else{
-		console.log('find ok!'+data);
-	}
-	console.log('data : '+data.length);
-    res.render('arrange/timetable_classroom', {
-        name: '程序员', 
-        image: 'images/avatars/avatar3.jpg',
-        total_a:'12',
-        a:'2,3,1,2,3,1,0',
-        total_b:'24',
-        b:'4,6,2,4,6,2,0',
-        total_credits:'24',
-        credits:'4,6,2,4,6,2,0',
-        course_data: data
-    });
+    CourseModel.findbyclassroom(req.body.campus,req.body.classid2, function(error,data){
+        if(error)
+    	{
+    		console.log('find error!'+error);
+    	}
+    	else{
+    		console.log('find ok!'+data);
+    	}
+    	console.log('data : '+data.length);
+        res.render('arrange/timetable_classroom', {
+            name: '程序员', 
+            image: 'images/avatars/avatar3.jpg',
+            total_a:'12',
+            a:'2,3,1,2,3,1,0',
+            total_b:'24',
+            b:'4,6,2,4,6,2,0',
+            total_credits:'24',
+            credits:'4,6,2,4,6,2,0',
+            course_data: data
+        });
     });
 });
+
+module.exports = router;
