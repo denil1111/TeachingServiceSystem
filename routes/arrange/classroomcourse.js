10e5d5056b53c34b253f6f7efaf79c781cc7c48e
HittaX <songzp.work.com>
2015-05-31 02:46:29 +0800

    x

--- routes/arrange/classroomcourse.js
+++ routes/arrange/classroomcourse.js
@@ -21,8 +21,8 @@ router.get('/classroomcourse',function(req,res,next){
         total_credits:'24',
         credits:'4,6,2,4,6,2,0',
 //        classroom_data: classroom,
-        classroom_arrange:tmp
-    	});
+        course_data:tmp
+    });
 });
 
 router.post('/classroomcourse',function(req,res,next){
@@ -34,133 +34,17 @@ router.post('/classroomcourse',function(req,res,next){
 		else{
 			console.log('find succeed!' + error);
 		}
+		res.render('arrange/classroomcourse',{
+    	name: '程序员', 
+    	image: 'images/avatars/avatar3.jpg',
+   		total_a:'12',
+  		a:'2,3,1,2,3,1,0',
+    	total_b:'24',
+   		b:'4,6,2,4,6,2,0',
+    	total_credits:'24',
+    	credits:'4,6,2,4,6,2,0',
 
-		ClassroomModel.findall(function(err,classroom_total_info){
-			var classroom = classroom_total_info;
-    	});
-
-		var classroomcourse_info = new Array(2);	//classroomcourse_info[0]\[1] means single week or double week
-		//classroomcourse_info[0][0] the second index means the day
-		//classroomcourse_info[0][0][0] the third index means the time
-		//the times is {1,2,3},{4,5},{6,7,8},{7,8},{9,10},{11,12,13},{11,12}
-
-
-		for(var i=0;i<classroomcourse_info.length;i++)
-			classroomcourse_info[i]=new Array(7);
-		for(i=0;i<classroomcourse_info[0].length;i++)
-			classroomcourse_info[0][i]=new Array(7);
-		for(i=0;i<classroomcourse_info[1].length;i++)
-			classroomcourse_info[1][i]=new Array(7);
-
-		data.each(function(err,doc){
-			assert.equal(err,null);
-			var temps=doc.coursetime;
-			var times=temps.split(";");
-			times.each(function(i,item){
-				var index3;
-				var index2;
-				if(item[0]=="O"){
-					index2=item[1];
-					if(item[4]!="1"){
-						if(item[3]=="1"){
-							index3=0;
-						}
-						else if(item[3]=="4"){
-							index3=1;
-						}
-						else if(item[3]=="6"){
-							index3=2;
-						}
-						else if(item[3]=="7"){
-							index3=3;
-						}
-						else{
-							index3=4;
-						}
-					}
-					else{
-						if(item[10]=="3"){			//Ox-11,12,13
-							index3=5;
-						}	
-						else{
-							index3=6;
-						}
-					}
-					classroomcourse_info[0][index2][index3]=doc.coursename+doc.room;
-				}
-				else if(item[0]=="E"){
-					index2=item[1];
-					if(item[4]!="1"){
-						if(item[3]=="1"){
-							index3=0;
-						}
-						else if(item[3]=="4"){
-							index3=1;
-						}
-						else if(item[3]=="6"){
-							index3=2;
-						}
-						else if(item[3]=="7"){
-							index3=3;
-						}
-						else{
-							index3=4;
-						}
-					}
-					else{
-						if(item[10]=="3"){			//Ox-11,12,13
-							index3=5;
-						}	
-						else{
-							index3=6;
-						}
-					}
-					classroomcourse_info[1][index2][index3]=doc.coursename+doc.room;
-				}
-				else{
-					index2=item[0];
-					if(item[3]!="1"){
-						if(item[2]=="1"){
-							index3=0;
-						}
-						else if(item[2]=="4"){
-							index3=1;
-						}
-						else if(item[2]=="6"){
-							index3=2;
-						}
-						else if(item[2]=="7"){
-							index3=3;
-						}
-						else{
-							index3=4;
-						}
-					}
-					else{
-						if(item[9]=="3"){			//Ox-11,12,13
-							index3=5;
-						}	
-						else{
-							index3=6;
-						}
-					}
-					classroomcourse_info[1][index2][index3]=doc.coursename+doc.room;
-					classroomcourse_info[0][index2][index3]=doc.coursename+doc.room;
-				}
-			});
-			res.render('arrange/classroomcourse',{
-        	name: '程序员', 
-        	image: 'images/avatars/avatar3.jpg',
-       		total_a:'12',
-      		a:'2,3,1,2,3,1,0',
-        	total_b:'24',
-       		b:'4,6,2,4,6,2,0',
-        	total_credits:'24',
-        	credits:'4,6,2,4,6,2,0',
-
-        	classroom_data: classroom,
-        	classroom_arrange: classroomcourse_info,
-			});
+		course_data : data
 		});
 	});
 });
