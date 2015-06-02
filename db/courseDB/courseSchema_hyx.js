468185e2ff51daf186cd1ae837e16a351deb6975
zjuhyx <bright_yixin@126.com>
2015-06-02 22:33:16 +0800

    2015-06-02 changes to PLANCOURSE schema!!!

--- db/courseDB/courseSchema_hyx.js
+++ db/courseDB/courseSchema_hyx.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+
 var courseSchema_hyx = new mongoose.Schema({
 	id:{type:String},
 	name:{type:String},
@@ -6,7 +7,40 @@ var courseSchema_hyx = new mongoose.Schema({
 	credit:{type:Number},
 	type:{type:Number},//1-公共课；2-专业必修；3-专业选修
 	subtype:{type:String},
-	major:{type:String}
+	major:{type:String}//就是开课学院
 });
-var courseModel_hyx = mongoose.model('courseModel_hyx',courseSchema_hyx,'course');
+/*
+改：time->rectime
+增：
+classid []
+examtime
+
+再加一个class表（classSchema）
+classid id_1,id_2,...
+teacher
+room []
+time []
+campus
+*/
+
+CourseSchema.statics.insert = function(req, callback) {
+	var doc = {
+		id : req.id, 
+		name : req.name, 
+		time:req.time,
+		credit:req.credit,
+		type:req.type,
+		subtype:req.subtype,
+		major:req.major
+	};
+    return this.model('courseModel_hyx').create(doc, function(error){
+	    if(error) {
+	        console.log(error);
+	    } else {
+	        console.log('save ok');
+	    }
+	});
+}
+
+var courseModel_hyx = mongoose.model('courseModel_hyx',courseSchema_hyx,'plancourse');
 module.exports=courseModel_hyx;
