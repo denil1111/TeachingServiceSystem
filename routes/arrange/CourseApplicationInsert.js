f85ef60ea5bea35e3422cc3a91c9601fe7bf7014
Rex_Wei <328425665@qq.com>
2015-06-03 22:24:49 +0800

    ApplicationDB

---
+++ routes/arrange/CourseApplicationInsert.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var PersonModel = require('../../db/group1db/PersonModel');
+var CourseModel = require('../../db/group1db/CourseModel');
+var ClassroomModel = require('../../db/group2db/ClassroomModel');
+var CourseApplicationModel = require('../../db//group2db/CourseApplicationModel');
+
+var tmp=[];
+
+
+module.exports = router;
