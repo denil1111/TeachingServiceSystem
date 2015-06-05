c2c490a04b4f1180b4f8be0b262336cb16397236
HittaX <songzp.work.com>
2015-06-05 22:50:39 +0800

    delete some mistakes, add my timetable(search by teacher)

--- shabi/db/courseDB/courseSelectModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var courseSelectSchema = new mongoose.Schema({
-	id:{type:String},
-	remain:{type:Number},
-	all:{type:Number},
-	waiting:{type:Number}
-});
-var courseSelectModel = mongoose.model('courseSelectModel',courseSelectSchema,'coursesSelects');
-module.exports=courseSelectModel;
