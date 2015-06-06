a97b574626363a9230a09c58f87c7c71ccbb8888
dodoleon <gaotaohu@163.com>
2015-06-06 10:04:15 +0800

    homwwork model

--- db/resource/homework.js
+++ db/resource/homework.js
@@ -1,13 +1,49 @@
 // created by gaotao
 var mongoose = require('mongoose');
 // Schema 结构
+var upfileSchema = new mongoose.Schema({
+    stid        : {type : String},
+    filename    : {type : String},
+    contentType : {type : String},
+    id          : {type : String}
+});
+
+var homeworkSchema = new mongoose.Schema({
+    homework : {type : String},
+    uploadfile : [upfileSchema]
+});
+
 var mongooseSchema = new mongoose.Schema({
     courseid : {type : String},
-    homework : [], //{homework:"hw1",uploadfile:[{stid:"std1",fileid:"fid"}]}
+    homeworklist : [homeworkSchema] //{homework:"hw1",uploadfile:[{stid:"std1",fileinfo:"fid"}]}
 });
-mongooseSchema.statics.findbycourseid = function(courseid, callback) {
-    return this.model('homework').find({courseid: courseid}, callback);
-};
+var collectionname = 'homework';
 
-var mongooseModel = mongoose.model('homework',mongooseSchema);
+mongooseSchema.statics.insertdemo = function (callback) {
+    var homework1 = {
+        homework : 'hw1',
+        uploadfile : []
+    }
+    var homework2 = {
+        homework : 'hw2',
+        uploadfile : []
+    }
+    var course = {
+        courseid : 'g1',
+        homeworklist : [homework1,homework2]
+    }
+    return this.model('homeworkModel').create(course,callback);
+}
+
+mongooseSchema.statics.findbycourseid = function(courseid, callback) {
+    return this.model('homeworkModel').find({courseid: courseid}, callback);
+};
+mongooseSchema.statics.updatehw = function(cid, homework, callback) {
+    console.log('update');
+    var conditions = {courseid: cid};
+	var update     = {$set : {homeworklist: homework}};
+	var options    = {upsert : true};
+    return this.model('homeworkModel').update(conditions,update,options,callback);
+};
+var mongooseModel = mongoose.model('homeworkModel',mongooseSchema,collectionname);
 module.exports=mongooseModel;
--- routes/resource/course.js
+++ routes/resource/course.js
@@ -21,8 +21,8 @@ function getCourseList(userid, callback) {
       var courseList = _courseList ? _courseList : [];
       debug(courseList);
       callback(err, courseList);
-    })
-  })
+    });
+  });
 }
 
 
@@ -35,7 +35,7 @@ function isValidCourseID(req, res, next) {
     debug(JSON.stringify(req.session.courseList));
     var cList = req.session.courseList;
     var in_flag = false;
-    for(var i = 0; i < cList.length; i++) {
+    for (var i = 0; i < cList.length; i++) {
       if (req.query.cid === cList[i].courseid2) {
         in_flag = true;
         break;
@@ -59,7 +59,7 @@ router.use(
     if ('courseList' in req.session) {
       next();
     } else {
-      getCourseList(req.session.user.userid, function(err, courseList) {
+      getCourseList(req.session.user.userid, function (err, courseList) {
         if (err)
           next(err);
         else {
@@ -75,7 +75,7 @@ router.use(
     if (!('slide_course' in req.session)) {
       var arr = [];
       debug('arr length is ' + arr.length);
-      for(var i = 0; i < req.session.courseList.length; i++) {
+      for (var i = 0; i < req.session.courseList.length; i++) {
         var c = req.session.courseList[i];
         debug('arr at ' + i + ' is ' + c);
         arr.push({
@@ -91,7 +91,7 @@ router.use(
     }
     next();
   }
-);
+  );
 
 router.get('/', function (req, res, next) {
   res.redirect('/resource/course/data');
@@ -99,9 +99,9 @@ router.get('/', function (req, res, next) {
 
 router.get('/data', isValidCourseID, function (req, res, next) {
   var render_data = {
-    current_cid   : decodeURIComponent(req.query.cid),
-    slide_course  : req.session.slide_course,
-    path_prefix   : 'data'
+    current_cid: decodeURIComponent(req.query.cid),
+    slide_course: req.session.slide_course,
+    path_prefix: 'data'
   };
   debug(render_data);
   res.render('resource/course_data', render_data);
@@ -109,16 +109,16 @@ router.get('/data', isValidCourseID, function (req, res, next) {
 
 router.get('/info', isValidCourseID, function (req, res, next) {
   var render_data = {
-    current_cid   : decodeURIComponent(req.query.cid),
-    slide_course  : req.session.slide_course,
-    path_prefix   : 'info'
+    current_cid: decodeURIComponent(req.query.cid),
+    slide_course: req.session.slide_course,
+    path_prefix: 'info'
   };
 
   res.render('resource/course_info', render_data);
 });
 
-router.get('/homework/upload', function(req, res, next) {
-  var html = '<form action="/resource/cloud/upload"enctype="multipart/form-data" method="post"> ' +
+router.get('/homework/upload', function (req, res, next) {
+  var html = '<form action="/resource/course/homework/upload?cid=g1&hw=hw1"enctype="multipart/form-data" method="post"> ' +
     '<h1> Upload your file </h1> ' +
     'Please specify a file, or a set of files:<br> ' +
     '<input type="file" name="file" size="40" multiple="multiple">  ' +
@@ -131,33 +131,47 @@ router.get('/homework/upload', function(req, res, next) {
   file upload api
 
  */
-router.post('/homework/upload', function(req, res, next) {
-  File.upload(req,function (id) {
-    console.log(id);
+router.get('/homework/insertdemo', function (req, res, next) {
+  homeworkModel.insertdemo(function (error, doc) {
+    console.log(doc);
   });
-  console.log(req);
-//  req.busboy.on('file', function(fieldname, readStream, filename, encoding, mimetype) {
-//    debug('a file is posted: ' + filename);
-//    var ws = gfs.createWriteStream({
-//      mode: 'w',
-//      content_type: mimetype,
-//      filename: filename,
-//      metadata: {}
-//    });
-//    console.log(ws.id);
-//    readStream.pipe(ws);
-//  });
-
-  //TODO: should not allow any other field to be post to the upload route
-  req.busboy.on('field', function(key, value, keyTruncated, valueTruncated) {
-    debug('Field is ' + key + value);
+})
+router.post('/homework/upload', function (req, res, next) {
+  var cid = decodeURIComponent(req.query.cid);
+  var homework = decodeURIComponent(req.query.hw);
+  File.upload(req, function (fileinfo) {
+    console.log(fileinfo);
+    var file = {
+      stid: req.session.user.userid,
+      filename: fileinfo.name,
+      contentType: fileinfo.options.content_type,
+      id: fileinfo.id
+    };
+    console.log("file");
+    console.log(file);
+    homeworkModel.findbycourseid(cid, function (error, result) {
+      if (error) {
+        console.log(error);
+      } else {
+        var homeworkList = result[0].homeworklist;
+        for (var i = 0; i < homeworkList.length; i++) {
+          if (homeworkList[i].homework == homework) {
+            homeworkList[i].uploadfile.push(file);
+            break;
+          }
+        };
+        homeworkModel.updatehw(cid, homeworkList, function (error, doc) {
+          console.log('update');
+          console.log(doc);
+          if (error) {
+            console.log(error);
+          } else {
+            res.redirect('/resource/course/data');
+          }
+        });
+      }
+    });
   });
-
-  req.busboy.on('finish', function() {
-    res.redirect('/resource/cloud');
-  });
-//
-//  req.pipe(req.busboy);
 });
 
 router.get('/homework/', isValidCourseID, function (req, res, next) {
@@ -165,43 +179,64 @@ router.get('/homework/', isValidCourseID, function (req, res, next) {
   var homeworkName = [];
   var filelist = [];
   var thisfilelist = [];
+  var thisuploadfile = [];
+  var fileinfo;
   Person.findbyid(req.session.user.userid, function (err, user) {
-    homeworkModel.findbycourseid(cid, function (error,result) {
-      var homeworkList=result.homework;
-      for (var i=0; i<homeworkList.length; i++) {
-        homeworkName.push(homeworkList.homework);
-        var uploadfile=homeworkList.uploadfile;
-        thisfilelist=[];
-        for (var j=0;j<uploadfile.length;j++){
-          if (user.status == '学生') {
-            if (uploadfile.stid == user.userid){
-              thisfilelist.push(uploadfile.fileid);
+    homeworkModel.findbycourseid(cid, function (error, result) {
+      if (error) {
+        console.log(error);
+      } else {
+        var homeworkList = result[0].homeworklist;
+        for (var i = 0; i < homeworkList.length; i++) {
+          homeworkName.push(homeworkList[i].homework);
+          debug('homeworkName:');
+          debug(homeworkName);
+          thisuploadfile = homeworkList[i].uploadfile;
+          debug('uploadfile:');
+          debug(thisuploadfile);
+          thisfilelist = [];
+          for (var j = 0; j < thisuploadfile.length; j++) {
+            fileinfo = {
+              filename: thisuploadfile[j].filename,
+              contentType: thisuploadfile[j].contentType,
+              id: thisuploadfile[j].id
+            };
+            if (user.status == '学生') {
+              if (thisuploadfile[j].stid == user.userid) {
+                thisfilelist.push(fileinfo);
+              }
+            } else {
+              thisfilelist.push(fileinfo);
             }
+          }
+          filelist.push(thisfilelist);
+          if (i == homeworkList.length - 1) {
+            console.log('true');
+            var render_data = {
+              homewokeLisr: homeworkName,
+              uploadfile: filelist,//是一个二维数组
+              current_cid: decodeURIComponent(req.query.cid),
+              slide_course: req.session.slide_course,
+              path_prefix: 'homework'
+            };
+            debug('homework');
+            debug(filelist);
+            res.render('resource/course_homework', render_data);
           } else {
-            thisfilelist.push(uploadfile.fileid);
+            console.log(i);
           }
         }
-        filelist.push(thisfilelist);
       }
     });
-      //课程名单放在了homeworkName,上传的文件在fileidlist中   
+    //课程名单放在了homeworkName,上传的文件在fileidlist中   
   });
-  var render_data = {
-    homewokeLisr  : homeworkName,
-    uploadfile    : filelist,//是一个二维数组
-    current_cid   : decodeURIComponent(req.query.cid),
-    slide_course  : req.session.slide_course,
-    path_prefix   : 'homework'
-  };
-
-  res.render('resource/course_homework', render_data);
 });
 
 router.get('/feedback', function (req, res, next) {
   var render_data = {
-    current_cid   : decodeURIComponent(req.query.cid),
-    slide_course  : req.session.slide_course,
-    path_prefix   : 'feedback'
+    current_cid: decodeURIComponent(req.query.cid),
+    slide_course: req.session.slide_course,
+    path_prefix: 'feedback'
   };
 
   res.render('resource/course_feedback', render_data);
