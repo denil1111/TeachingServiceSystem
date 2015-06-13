8fda62d614825bd4505f221fa9d50c882463017d
yhyhyh <893132051@qq.com>
2015-06-13 17:06:12 +0800

    show course table in teachers' view

--- routes/select/courseTable.js
+++ routes/select/courseTable.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 
-var my_course=[];
-my_course.push({
+state = 1;  // 1:teacher, 0:student
+/*my_course.push({
   course_id:"000000",
   ID:"00001",
   name:'计算机组成',
@@ -13,14 +13,15 @@ my_course.push({
   time:'周一 345',
   campus:'玉泉',
   room:'曹西-204'
-});
+});*/
 //注意所有课程的time日期格式，多个上课时间之间以空格分隔，共有以下情况：
 //晚上的课程格式为 Mon night
 //其余为 周一 123 或 周一 12 以此类推
 //新增course_id为主键, 
-my_course.push({course_id:"000001",ID:"00002",name:'软件工程',teacher:'XX2', semaster:'春夏',time:'周一 12 周二 345',campus:'玉泉',room:'曹西-101'});
+//my_course.push({course_id:"000001",ID:"00002",name:'软件工程',teacher:'XX2', semaster:'春夏',time:'周一 12 周二 345',campus:'玉泉',room:'曹西-101'});
 /* GET home page. */
 router.get('/my_course', function(req, res, next) {
+  var my_course=[];
   console.log(my_course.ejs);
   res.render('select/my_course', {
     type:1,//manager
@@ -36,6 +37,7 @@ router.get('/my_course', function(req, res, next) {
     start_year:'2013',/*入学年份*/
     this_year:'2015',/*今年的年份*/
     this_semester:'春',
+    error:""
   });
 });
 
@@ -52,73 +54,6 @@ router.post('/my_course', function(req, res, next) {
     //     }
     //关闭数据库链接
 
-    var userModel = require('../../db/courseDB/userSchema'); 
-    userModel.find({id: "u001"}, function(error,raw_result){
-    if(error) {
-        console.log(error);
-    } else {
-        my_course_list = [];        
-        if (raw_result.length!=0)
-            my_course_list = raw_result[0].confirmedCourse;
-        console.log(my_course_list);
-        console.log('1');
-    }
-    var courseModel = require('../../db/group1db/CourseModel'); 
-    my_course = [];
-    for (var i=0;i<my_course_list.length;i++)
-    {
-        console.log(my_course_list[i].id);
-        (function(i){ 
-            courseModel.find({_id:my_course_list[i].id},function(error,nresult){
-            if(error) {
-                console.log(error);
-            } else {
-                console.log(nresult);
-            }              
-            if (nresult.length!=0)
-                my_course.push({course_id:"00010", ID:nresult[0].courseid2, name:nresult[0].coursename, teacher:nresult[0].teacher, semaster:nresult[0].courseterm, time:nresult[0].coursetime, campus:nresult[0].campus, room:nresult[0].room});           
-            
-            console.log("!");
-            if (my_course.length==my_course_list.length){
-            console.log(my_course);
-            res.render('select/my_course', {
-                type:2,//manager
-                name: '程序员', 
-                image: 'images/avatars/avatar3.jpg',
-                total_a:'12',
-                a:'2,3,1,2,3,1,0',
-                total_b:'24',
-                b:'4,6,2,4,6,2,0',
-                total_credits:'24',
-                credits:'4,6,2,4,6,2,0',
-                course_data: my_course,
-                start_year:'2013',
-                this_year:'2015',
-                this_semester:'春'
-            });
-            }
-        });
-        })(i); 
-    }
-    res.render('select/my_course', {
-                type:2,//manager
-                name: '程序员', 
-                image: 'images/avatars/avatar3.jpg',
-                total_a:'12',
-                a:'2,3,1,2,3,1,0',
-                total_b:'24',
-                b:'4,6,2,4,6,2,0',
-                total_credits:'24',
-                credits:'4,6,2,4,6,2,0',
-                course_data: my_course,
-                start_year:'2013',
-                this_year:'2015',
-                this_semester:'春'
-            });
-    console.log('2');
-      // db.close();
-    });
-console.log('3');
 });
 
 //课程人员列表
@@ -126,6 +61,7 @@ console.log('3');
 students.push({sId:"3130000027",sname:"桓神",classNo:"启真1301",major:"计算机科学与技术"});
 students.push({sId:"3130000017",sname:"闻神",classNo:"启真1301",major:"计算机科学与技术"});*/
 router.get('/course_list/:courseID', function(req, res, next){
+  var my_course=[];
   //课程号
   var course_id = req.params.courseID;
   var courseStudentModel=require('../../db/courseDB/courseStudentSchema');
@@ -206,21 +142,25 @@ router.get('/course_list/:courseID', function(req, res, next){
             });
         }
   });
-  
-  
-
 });
 
-
-
-
-
-
 router.get('/my_course/:timeID',function(req, res, next){
+    var my_course=[];
     var time=req.params.timeID;
     console.log(time);
-    res.render('select/my_course', {
-                  type:2,//manager
+
+    var userModel = require('../../db/courseDB/userSchema'); 
+    var courseModel = require('../../db/group1db/CourseModel'); 
+    if (state==1){
+        courseModel.find({teacher:"Henry"}, function(error, jresult){
+            if (error)
+                console.log(error);
+            else {
+                for (var i=0;i<jresult.length;i++)
+                    my_course.push({course_id:"00010", ID:jresult[i].courseid2, name:jresult[i].coursename, teacher:jresult[i].teacher, semaster:jresult[i].courseterm, time:jresult[i].coursetime, campus:jresult[i].campus, room:jresult[i].room});
+                console.log(my_course);
+                res.render('select/my_course', {
+                type:1,//manager
                 name: '程序员', 
                 image: 'images/avatars/avatar3.jpg',
                 total_a:'12',
@@ -233,11 +173,63 @@ router.get('/my_course/:timeID',function(req, res, next){
                 start_year:'2013',
                 this_year:'2015',
                 this_semester:'春',
-                error:"查询错误！"
-                //注意start_year是学生进校的第一学年
-                //this year就是学生查询的年份
-                //this_semester就是学生查询的学期
-                //error是错误信息
-        }); 
+                error:""
+                });
+            }
+        });
+    }
+    else {
+        userModel.find({id: "u001"}, function(error,raw_result){
+        if(error) {
+            console.log(error);
+        } else {
+            my_course_list = [];        
+            if (raw_result.length!=0)
+                my_course_list = raw_result[0].confirmedCourse;
+            console.log(my_course_list);
+            console.log('1');
+        }
+        my_course = [];
+        for (var i=0;i<my_course_list.length;i++)
+        {
+            console.log(my_course_list[i].id);
+            (function(i){ 
+                courseModel.find({_id:my_course_list[i].id},function(error,nresult){
+                if(error) {
+                    console.log(error);
+                } else {
+                    console.log(nresult);
+                }              
+                if (nresult.length!=0)
+                    my_course.push({course_id:"00010", ID:nresult[0].courseid2, name:nresult[0].coursename, teacher:nresult[0].teacher, semaster:nresult[0].courseterm, time:nresult[0].coursetime, campus:nresult[0].campus, room:nresult[0].room});           
+                
+                console.log("!");
+                if (my_course.length==my_course_list.length){
+                console.log(my_course);
+                res.render('select/my_course', {
+                    type:2,//manager
+                    name: '程序员', 
+                    image: 'images/avatars/avatar3.jpg',
+                    total_a:'12',
+                    a:'2,3,1,2,3,1,0',
+                    total_b:'24',
+                    b:'4,6,2,4,6,2,0',
+                    total_credits:'24',
+                    credits:'4,6,2,4,6,2,0',
+                    course_data: my_course,
+                    start_year:'2013',
+                    this_year:'2015',
+                    this_semester:'春',
+                    error:""
+                });
+                }
+            });
+            })(i); 
+        }
+        console.log('2');
+          // db.close();
+        });
+    }
+
 });
 module.exports = router;
