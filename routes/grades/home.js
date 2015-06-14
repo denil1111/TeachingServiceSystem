67eac2acd81ec00576aa1525478a69bed2425584
Martin <marquezx@163.com>
2015-06-15 00:38:55 +0800

    fix bugs

--- routes/grades/home.js
+++ routes/grades/home.js
@@ -156,29 +156,5 @@ else if(req.session.user.status=="admin"){
 
 });  
 
-router.get('/gradesAnalysis',function(req, res, next) {
-
-if(!req.session.user){return res.redirect('../info/login');}
-
-
-gradesDB.find(function(error,docs){
-    if(error){
-        console.log(error);
-        return;
-    }
-
-  res.render('grades/student_analysis', {
-  	name: '程序员', 
-  	image: 'images/avatars/avatar1.jpg',
-  	total_a:'12',
-  	a:'2,3,1,2,3,1,0',
-  	total_b:'24',
-  	b:'4,6,2,4,6,2,0',
-  	total_credits:'24',
-  	credits:'4,6,2,4,6,2,0'
-  });
-  }); 
-});  
-
 
 module.exports =router;
